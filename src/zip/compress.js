e0b754de0874c6198d184029688f80e2a121bb3d
Macadamandi <vlad.yurchenko.2024@gmail.com>
2025-10-21 19:11:08 +0300

    feat: implement zip subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import { createGzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import showError from "../helpers/showError.js";
+import getDir from "../helpers/getDir.js";
+import getPath from "../helpers/getPath.js";
+
 const compress = async () => {
-  // Write your code here
+  try {
+    const currentDir = getDir(import.meta.url);
+    const filePathToCompress = getPath(currentDir, [".", "files", "fileToCompress.txt"]);
+    const compressedFilePath = getPath(currentDir, [".", "files", "archive.gz"]);
+
+    const source = createReadStream(filePathToCompress);
+    const destination = createWriteStream(compressedFilePath);
+
+    const gzip = createGzip();
+
+    await pipeline(source, gzip, destination);
+
+    console.log(`Compressed file: \n${compressedFilePath}`);
+  } catch (err) {
+    showError(err.message);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,29 @@
+import { createGunzip } from "node:zlib";
+import { createReadStream, createWriteStream } from "node:fs";
+import { access, constants } from "node:fs/promises";
+import { pipeline } from "node:stream/promises";
+import showError from "../helpers/showError.js";
+import getDir from "../helpers/getDir.js";
+import getPath from "../helpers/getPath.js";
+
 const decompress = async () => {
-  // Write your code here
+  try {
+    const currentDir = getDir(import.meta.url);
+    const filePathToDecompress = getPath(currentDir, [".", "files", "archive.gz"]);
+    const decompressedFilePath = getPath(currentDir, [".", "files", "fileToCompress.txt"]);
+
+    const source = createReadStream(filePathToDecompress);
+    const destination = createWriteStream(decompressedFilePath);
+
+    const gunzip = createGunzip();
+
+    await access(filePathToDecompress, constants.F_OK);
+    await pipeline(source, gunzip, destination);
+
+    console.log(`Decompressed file: \n${decompressedFilePath}`);
+  } catch (err) {
+    showError(err.message);
+  }
 };
 
 await decompress();
