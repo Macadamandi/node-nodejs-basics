02ce5f2caaecd7e192ea947393e52c720cdb352c
Macadamandi <vlad.yurchenko.2024@gmail.com>
2025-10-21 16:21:56 +0300

    feat: implement command line interface subtask

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-  // Write your code here
+  const envObj = process.env;
+  const prefix = "RSS_";
+
+  const result = Object.entries(envObj)
+    .filter(([key]) => key.includes(prefix))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log(result);
 };
 
 parseEnv();
