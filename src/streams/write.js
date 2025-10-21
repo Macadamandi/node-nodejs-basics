5991a3e8dde4ccb48f94f5e3e4f96a2a1f15d3df
Macadamandi <vlad.yurchenko.2024@gmail.com>
2025-10-21 18:17:09 +0300

    feat: implement streams subtask

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import showError from "../helpers/showError.js";
+import getDir from "../helpers/getDir.js";
+import getPath from "../helpers/getPath.js";
+
 const write = async () => {
-  // Write your code here
+  try {
+    const currentDir = getDir(import.meta.url);
+    const filePathToWrite = getPath(currentDir, [".", "files", "fileToWrite.txt"]);
+
+    await pipeline(process.stdin, createWriteStream(filePathToWrite));
+  } catch (err) {
+    showError(err.message);
+  }
 };
 
 await write();
